d5456bcbdf470c69fdd67fc17b6e59094e557bee
NaomiGonz <114747919+NaomiGonz@users.noreply.github.com>
2025-02-10 17:26:55 -0500

    Update firebase.js

--- webApp/src/tools/firebase.js
+++ webApp/src/tools/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4ynX4PMihe31U1FA3NM50QfERD1QSOFY",
-    authDomain: "ai-coffee-a5ad7.firebaseapp.com",
-    projectId: "ai-coffee-a5ad7",
-    storageBucket: "ai-coffee-a5ad7.firebasestorage.app",
-    messagingSenderId: "69058188119",
-    appId: "1:69058188119:web:e46951a7ba1919d95de412",
-    measurementId: "G-1L3RE5DBGV"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
